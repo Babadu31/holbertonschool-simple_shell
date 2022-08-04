c90a51a9b0f782a6c2097a05783cac3c767314d9
guenoel <guenooel@hotmail.com>
2022-08-04 14:14:53 +0200

    test task 2

--- simple_shell.c
+++ simple_shell.c
@@ -6,42 +6,37 @@
  * Return: End status
  */
 
-int main(void)
-{
-	ssize_t nread;
-	size_t len = 0;
-	int status = 0;
-	pid_t child;
-	char *del = " \n";
-	char *buf = calloc(64, sizeof(char));
-	char **args = NULL;
+int main(void) {
+  ssize_t nread;
+  size_t len = 0;
+  int status = 0;
+  pid_t child;
+  char *del = " \n";
+  char *buf = NULL;
+  char **args = NULL;
 
-	while ((nread = getline(&buf, &len, stdin)) != -1)
-	{
-		args = split(buf, del);
-		if (args == NULL || args[0] == NULL)
-			continue;
-		/*path_handler(args);*/
-		child = fork();
-		if (child == -1)
-		{
-			perror("Error:");
-			return (1);
-		}
-		if (child == 0)
-		{
-			if (args[0] && execve(args[0], args, environ) == -1)
-			{
-				perror("execve");
-				exit(EXIT_FAILURE);
-			}
-			free_array_memory(args);
-			free_memory(buf);
-		} else
-			wait(&status);
-	}
-	/*free_array_memory(path_array);
-	free_array_memory(args);
-	free_memory(buf);*/
-	return (0);
+  while ((nread = getline(&buf, &len, stdin)) != -1) {
+    args = split(buf, del);
+    if (args == NULL || args[0] == NULL)
+      continue;
+    /*path_handler(args);*/
+    child = fork();
+    if (child == -1) {
+      perror("Error:");
+      return (1);
+    }
+    if (child == 0) {
+      if (args[0] && execve(args[0], args, environ) == -1) {
+        perror("execve");
+        exit(EXIT_FAILURE);
+      }
+      free_array_memory(args);
+      free_memory(buf);
+    } else
+      wait(&status);
+  }
+  /*free_array_memory(path_array); */
+  free_array_memory(args);
+  free_memory(buf);
+  return (0);
 }
